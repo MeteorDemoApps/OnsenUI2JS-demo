0c28043b31e90002299cd826951fa8012b7f1042
cyclops24 <cyclops.dll@gmail.com>
2016-07-07 09:17:55 +0430

    add fonts import to layout

--- imports/ui/layouts/app-body.js
+++ imports/ui/layouts/app-body.js
@@ -1,8 +1,14 @@
 import './app-body.html';
 
+import 'onsenui/css/font_awesome/css/font-awesome.min.css';
+import 'onsenui/css/ionicons/css/ionicons.min.css';
+import 'onsenui/css/material-design-iconic-font/css/material-design-iconic-font.min.css';
+
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 // import ons from 'onsenui/js/onsenui.js';
 import ons from 'onsenui';
 // Thanks https://github.com/frankdiox to help for fix this issue: https://github.com/MeteorDemoApps/OnsenUI2JS-demo/issues/1
 ons.platform.select("android");
+
+
